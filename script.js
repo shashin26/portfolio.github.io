df74dd1232ac9abf5b046e8231e8687c0b221d49
shashin26 <nellivalasa.shashi@gmail.com>
2023-05-28 22:55:29 +0530

    28-05-2023  added some styling and animation to the headings

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-
 }
-
 
 //scroll sections 
 let sections = document.querySelectorAll('section');
@@ -26,6 +24,14 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            //active section for ANIMATION on scroll
+            sec.classList.add('show-animate');
+
+        }
+        //if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+
         }
     });
     //sticky header
@@ -34,4 +40,6 @@ window.onscroll = () => {
     header.classList.toggle('sticky',window.scrollY >100);
 
     //remove toggle icon and navbar when click navbar links(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
